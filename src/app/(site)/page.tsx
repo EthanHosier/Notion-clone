164594bf3093d90ccffbf7a0dda46fd8deccc688
Your Name <ethanjhosier@gmail.com>
2024-01-01 15:25:30 +0000

    finished landing page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,10 +4,14 @@ import Image from "next/image";
 import React from "react";
 import Banner from "../../../public/appBanner.png";
 import Cal from "../../../public/cal.png";
-import { CLIENTS, USERS } from "@/lib/constants";
+import Diamond from "../../../public/icons/diamond.svg";
+import CheckIcon from "../../../public/icons/check.svg";
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
 import { randomUUID } from "crypto";
 import { cn } from "@/lib/utils";
 import CustomCard from "@/components/landing-page/custom-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 
 const Homepage = () => {
   return (
@@ -19,7 +23,7 @@ const Homepage = () => {
         />
         <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
           <Button
-            variant="secondary"
+            variant="btn-secondary"
             className="w-full rounded-[10px] p-6 text-2xl bg-background"
           >
             Get Cypress Free
@@ -101,7 +105,7 @@ const Homepage = () => {
             <div
               key={randomUUID()}
               className={cn(
-                "mt-10 flex flex-nowrap",
+                "mt-10 flex flex-nowrap gap-6 self-start",
                 {
                   "flex-row-reverse": index === 1,
                   "animate-[slide_250s_linear_infinite]": true,
@@ -112,9 +116,103 @@ const Homepage = () => {
               )}
             >
               {USERS.map((testimonial, index) => (
-                <CustomCard key={testimonial.name}></CustomCard>
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px] shrink-0 rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background"
+                  cardHeader={
+                    <div className="flex items-center gap-4">
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }
+                ></CustomCard>
               ))}
             </div>
+          ))}
+        </div>
+      </section>
+      <section className="mt-20 px-4 sm:px-6">
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div className="flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10">
+          {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={card.planType}
+              className={cn(
+                "w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative",
+                {
+                  "border-brand-primaryBlue/70":
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle className="text-2xl font-semibold">
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div className="hidden dark:block w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/80 -z-10 top-0" />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span className="font-normal text-2xl">${card.price}</span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  <p className="dark:text-washed-purple=800">
+                    {card.description}
+                  </p>
+                  <Button
+                    className="whitespace-nowrap w-full mt-4"
+                    variant="btn-primary"
+                  >
+                    {card.planType === PRICING_PLANS.proplan
+                      ? "Go Pro"
+                      : "Get Started"}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul className="font-normal flex mb-2 flex-col gap-4">
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li key={feature} className="flex items-center gap-2">
+                      <Image src={CheckIcon} alt="Check Icon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
           ))}
         </div>
       </section>
--- src/components/landing-page/custom-card.tsx
+++ src/components/landing-page/custom-card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "../ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import { cn } from "@/lib/utils";
 
 type CardProps = React.ComponentProps<typeof Card>;
@@ -16,7 +16,13 @@ const CustomCard: React.FC<CustomCardProps> = ({
   cardFooter,
   ...props
 }) => {
-  return <Card className={cn("w-[380px]", className)} {...props}></Card>;
+  return (
+    <Card className={cn("w-[380px]", className)} {...props}>
+      <CardHeader>{cardHeader}</CardHeader>
+      <CardContent className="grid gap-4">{cardContent}</CardContent>
+      <CardFooter>{cardFooter}</CardFooter>
+    </Card>
+  );
 };
 
 export default CustomCard;
